184e0f83fda80cd998e9478989a36a779859808e
Cas <cassadeecreeks08@gmail.com>
2025-07-09 20:21:19 -0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// scripts.js
+
+// This file is included to meet project requirements.
+// You can add JavaScript functionality here in the future if needed.
+
+console.log("Cassadee Wilson's Resume Page scripts loaded successfully!");
